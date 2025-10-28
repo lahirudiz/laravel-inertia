0434af1755e5ca64f44dbbdcf30320822ba05d85
Lahiru <lahirudiz@live.com>
2025-10-28 22:32:57 +1300

    ADD Post delete function

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -67,6 +67,7 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
-        //
+        $post->delete();
+        return redirect('/');
     }
 }
--- resources/js/Pages/Show.jsx
+++ resources/js/Pages/Show.jsx
@@ -1,4 +1,11 @@
+import { useForm } from "@inertiajs/react";
+
 export default function Show({ post }) {
+    const { delete: destroy } = useForm();
+    function deletePost(e) {
+        e.preventDefault();
+        destroy(`/post/${post.id}`);
+    }
     return (
         <>
             <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50">
@@ -9,6 +16,11 @@ export default function Show({ post }) {
                     Posted on: {new Date(post.created_at).toLocaleTimeString()}
                 </span>
                 <p className="text-gray-700 text-lg">{post.body}</p>
+                <form onSubmit={deletePost}>
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+                        Delete
+                    </button>
+                </form>
             </div>
         </>
     );
